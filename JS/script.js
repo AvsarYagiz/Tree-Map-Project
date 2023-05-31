681e8a111ba8515570d502e4771fac906261a459
Yagiz Avsar <63153577+AvsarYagiz@users.noreply.github.com>
2023-05-31 15:05:50 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,158 @@
+// Define the URL for the JSON data
+let url = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
+
+// Declare the variable to store the movie data
+let movieData;
+
+// Select the canvas element using D3
+let canvas = d3.select("#canvas");
+
+// Define the function to draw the tree map
+let drawTreeMap = () => {
+    // Create a tooltip element
+    let tooltip = d3.select("body")
+        .append("div")
+        .attr("id", "tooltip")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden");
+
+    // Create a hierarchical structure of the movie data
+    let hierarchy = d3.hierarchy(movieData, (node) => {
+        return node["children"];
+    }).sum((node) => {
+        return node["value"];
+    }).sort((node1, node2) => {
+        return node2["value"] - node1["value"];
+    });
+
+    // Create a tree map layout
+    let createTreeMap = d3.treemap()
+        .size([1000, 600]);
+
+    // Generate the tree map layout based on the hierarchy
+    createTreeMap(hierarchy);
+
+    // Get the movie titles as leaves of the hierarchy
+    let movieTitles = hierarchy.leaves();
+
+    // Create a group element for each movie
+    let block = canvas.selectAll("g")
+        .data(movieTitles)
+        .enter()
+        .append("g")
+        .attr("transform", (movie) => {
+            return "translate(" + movie["x0"] + ", " + movie["y0"] + ")";
+        });
+
+    // Add rectangles for each movie as tiles
+    block.append("rect")
+        .attr("class", "tile")
+        .attr("stroke", "black")
+        .attr("fill", (movie) => {
+            let category = movie["data"]["category"];
+            if (category === "Action") {
+                return "firebrick";
+            } else if (category === "Drama") {
+                return "deepskyblue";
+            } else if (category === "Adventure") {
+                return "darkseagreen";
+            } else if (category === "Family") {
+                return "khaki";
+            } else if (category === "Animation") {
+                return "darkorchid";
+            } else if (category === "Comedy") {
+                return "orange";
+            } else if (category === "Biography") {
+                return "saddlebrown";
+            }
+        })
+        .attr("data-name", (movie) => {
+            return movie["data"]["name"];
+        })
+        .attr("data-category", (movie) => {
+            return movie["data"]["category"];
+        })
+        .attr("data-value", (movie) => {
+            return movie["data"]["value"];
+        })
+        .attr("width", (movie) => {
+            return movie["x1"] - movie["x0"];
+        })
+        .attr("height", (movie) => {
+            return movie["y1"] - movie["y0"];
+        })
+        .on("mouseover", (movie) => {
+            // Show the tooltip on mouseover
+            tooltip.transition()
+                .style("visibility", "visible");
+
+            // Set the content of the tooltip
+            tooltip.html(
+                "Name: " + movie["data"]["name"] + "<br/>" + "Category: " + movie["data"]["category"] + "<br/>" + "Value: $" + movie["data"]["value"]
+            );
+
+            // Set attributes on the tooltip element
+            document.querySelector("#tooltip").setAttribute("data-name", movie["data"]["name"]);
+            document.querySelector("#tooltip").setAttribute("data-category", movie["data"]["category"]);
+            document.querySelector("#tooltip").setAttribute("data-value", movie["data"]["value"]);
+        })
+        .on("mousemove", function () {
+            // Position the tooltip based on the mouse position
+            return tooltip.style("top", (d3.event.pageY - 20) + "px").style("left", (d3.event.pageX + 50) + "px");
+        })
+        .on("mouseout", (movie) => {
+            // Hide the tooltip on mouseout
+            tooltip.transition()
+                .style("visibility", "hidden");
+        });
+
+    // Add text labels for each movie
+    block.append("text")
+        .text((movie) => {
+            return movie["data"]["name"];
+        })
+        .attr("x", 5)
+        .attr("y", 20)
+        .attr("font-size", "10px")
+        .on("mouseover", (movie) => {
+            // Show the tooltip on mouseover
+            tooltip.transition()
+                .style("visibility", "visible");
+
+            // Set the content of the tooltip
+            tooltip.html(
+                "Name: " + movie["data"]["name"] + "<br/>" + "Category: " + movie["data"]["category"] + "<br/>" + "Value: $" + movie["data"]["value"]
+            );
+
+            // Set attributes on the tooltip element
+            document.querySelector("#tooltip").setAttribute("data-name", movie["data"]["name"]);
+            document.querySelector("#tooltip").setAttribute("data-category", movie["data"]["category"]);
+            document.querySelector("#tooltip").setAttribute("data-value", movie["data"]["value"]);
+        })
+        .on("mousemove", function () {
+            // Position the tooltip based on the mouse position
+            return tooltip.style("top", (d3.event.pageY - 20) + "px").style("left", (d3.event.pageX + 50) + "px");
+        })
+        .on("mouseout", (movie) => {
+            // Hide the tooltip on mouseout
+            tooltip.transition()
+                .style("visibility", "hidden");
+        });
+};
+
+// Load the JSON data
+d3.json(url).then(
+    (data, error) => {
+        if (error) {
+            console.log(error);
+        } else {
+            // Store the loaded movie data
+            movieData = data;
+            console.log(movieData);
+
+            // Call the drawTreeMap function to render the visualization
+            drawTreeMap();
+        }
+    }
+);
